d21d5d4b77fa85861c45c61c41b27b09a574e188
aaronli16 <liaaron53@gmail.com>
2025-03-02 14:47:20 -0800

    implemented disjoint sets

--- mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
+++ mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
@@ -1,6 +1,9 @@
 package disjointsets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A quick-union-by-size data structure with path compression.
@@ -9,6 +12,8 @@ import java.util.List;
 public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
     // Do NOT rename or delete this field. We will be inspecting it directly in our private tests.
     List<Integer> pointers;
+    Map<T, Integer> itemToIndex;
+
 
     /*
     However, feel free to add more fields and private helper methods. You will probably need to
@@ -16,25 +21,65 @@ public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
     */
 
     public UnionBySizeCompressingDisjointSets() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        pointers = new ArrayList<>();
+        itemToIndex = new HashMap<>();
+
     }
 
     @Override
     public void makeSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        pointers.add(-1);
+        itemToIndex.put(item, pointers.size() - 1);
+
     }
 
     @Override
     public int findSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (!itemToIndex.containsKey(item)) {
+            throw new IllegalArgumentException("Item not found in any set");
+        }
+        int index= itemToIndex.get(item);
+        List<Integer> vistedItems = new ArrayList<>();
+
+        while (pointers.get(index) >= 0) {
+            vistedItems.add(index);
+            index = pointers.get(index);
+        }
+
+        for (int i : vistedItems) {
+            pointers.set(i, index);
+        }
+        return index;
     }
 
     @Override
     public boolean union(T item1, T item2) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int rootA = findSet(item1);
+        int rootB = findSet(item2);
+        if (rootA == rootB) {
+            return false;
+        }
+        int weightA = -1 * pointers.get(rootA);
+        int weightB = -1 * pointers.get(rootB);
+
+        if (weightA > weightB) {
+            pointers.set(rootA, -1 * (weightA + weightB));
+            pointers.set(rootB, rootA);
+        }
+        else if (weightB > weightA) {
+            pointers.set(rootB, -1 * (weightA + weightB));
+            pointers.set(rootA, rootB);
+        }
+        else {
+            pointers.set(rootA, -1 * (weightA + weightB));
+            pointers.set(rootB, rootA);
+        }
+
+        return true;
+
+
+
+
+
     }
 }
