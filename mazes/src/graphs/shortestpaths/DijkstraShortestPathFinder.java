b48b5e2d905a01da64576be8af1f250a3bdb5856
aaronli16 <liaaron53@gmail.com>
2025-03-09 23:08:09 -0700

    implemented dijkstras

--- mazes/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
+++ mazes/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
@@ -5,7 +5,14 @@ import graphs.Graph;
 import priorityqueues.DoubleMapMinPQ;
 import priorityqueues.ExtrinsicMinPQ;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Objects;
 
 /**
  * Computes shortest paths using Dijkstra's algorithm.
@@ -31,14 +38,77 @@ public class DijkstraShortestPathFinder<G extends Graph<V, E>, V, E extends Base
 
     @Override
     protected Map<V, E> constructShortestPathsTree(G graph, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Map<V, E> edgeTo = new HashMap<>();
+        if (Objects.equals(start, end)) {
+            return edgeTo;
+        }
+        Map<V, Double> distTo = new HashMap<>();
+        Set<V> known = new HashSet<>();
+        ExtrinsicMinPQ<V> pq = new DoubleMapMinPQ<>();
+
+        pq.add(start, 0.0);
+        distTo.put(start, 0.0);
+        boolean foundEnd = false;
+        while (!pq.isEmpty() && !foundEnd) {
+            V u = pq.removeMin();
+            if (u.equals(end)) {
+                foundEnd = true;
+            } else {
+                known.add(u);
+
+                for (E edge : graph.outgoingEdgesFrom(u)) {
+                    V v = edge.to();
+                    double weight = edge.weight();
+                    if (!distTo.containsKey(v)) {
+                        distTo.put(v, Double.POSITIVE_INFINITY);
+                    }
+
+                    double oldDist = distTo.get(v);
+                    double newDist = distTo.get(u) + weight;
+                    if (newDist < oldDist) {
+                        distTo.put(v, newDist);
+                        edgeTo.put(v, edge);
+
+                        if (pq.contains(v)) {
+                            pq.changePriority(v, newDist);
+                        } else {
+                            pq.add(v, newDist);
+                        }
+                    }
+
+                }
+
+            }
+        }
+        return edgeTo;
     }
 
     @Override
     protected ShortestPath<V, E> extractShortestPath(Map<V, E> spt, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (Objects.equals(start, end)) {
+            return new ShortestPath.SingleVertex<>(start);
+        }
+        E edge = spt.get(end);
+
+        if (edge == null) {
+            return new ShortestPath.Failure<>();
+        }
+
+
+        List<E> path = new ArrayList<>();
+        V curr = end;
+
+        while (!Objects.equals(start, curr)) {
+            edge = spt.get(curr);
+            path.add(edge);
+            curr = edge.from();
+        }
+        Collections.reverse(path);
+        return new ShortestPath.Success<>(path);
+
+
+
+
     }
 
 }
